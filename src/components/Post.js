11c4e43310c6f62bc3f58c4081250eb07f879580
jagdish47 <jagdishkumawat81@gmail.com>
2023-10-17 11:32:20 +0530

    create the header and image section of post

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,7 +1,25 @@
+import { BsThreeDots } from "react-icons/bs";
+
 const Post = ({ post }) => {
   return (
-    <div>
-      <h1>{post.username}</h1>
+    <div className="bg-white my-7 border rounded-md">
+      {/* Post Header */}
+      <div className="flex items-center p-5">
+        <img
+          className="h-12 w-12 rounded-full object-cover border p-1 mr-3"
+          src={post?.userImg}
+          alt={post.username}
+        />
+        <p className="font-bold flex-1">{post?.username}</p>
+        <BsThreeDots className="h-5" />
+      </div>
+
+      {/* Post Image */}
+      <img
+        className="object-cover w-full"
+        src={post?.img}
+        alt={post?.username}
+      />
     </div>
   );
 };
--- src/components/Posts.js
+++ src/components/Posts.js
@@ -5,7 +5,8 @@ const Posts = () => {
     {
       id: "1",
       username: "codewithJassii",
-      userImg: "https://asset.brandfetch.io/ido5G85nya/idjNzcWu9R.png",
+      userImg:
+        "https://images.unsplash.com/photo-1575936123452-b67c3203c357?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8fDA%3D&w=1000&q=80",
       img: "https://images.unsplash.com/photo-1696635102402-a75df7af23a8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80",
       caption: "Nice Picture",
     },
